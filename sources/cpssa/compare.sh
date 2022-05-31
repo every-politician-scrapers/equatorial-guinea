0cd95f3256d5d87bd853f57582c5507b429617a4
Tony Bowden <tony@tmtm.com>
2022-05-31 11:46:42 +0300

    turn off CPSSA and Harvard comparisons
    These are static sources, so there's no need to keep running them

--- sources/cpssa/compare.sh
+++ sources/cpssa/compare.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# This is a static source that has already been imported in full, so there's no
+# need to keep running the comparison
+
 cd $(dirname $0)
 
-qsv join --left position cpssa.csv raw reconciled-positions.csv | qsv select minister,position\[1\],raw,startdate,enddate | qsv join --left minister - name reconciled-people.csv | qsv select id,minister,position,raw,startdate,enddate | qsv rename item,name,positionItem,position,start,end > scraped.csv
+# qsv join --left position cpssa.csv raw reconciled-positions.csv | qsv select minister,position\[1\],raw,startdate,enddate | qsv join --left minister - name reconciled-people.csv | qsv select id,minister,position,raw,startdate,enddate | qsv rename item,name,positionItem,position,start,end > scraped.csv
 
-wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | qsv search -s start . > wikidata.csv
+# wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | qsv search -s start . > wikidata.csv
 
-bundle exec ruby diff.rb | qsv sort -s name,position | tee diff.csv
+# bundle exec ruby diff.rb | qsv sort -s name,position | tee diff.csv
 
 cd ~-
--- sources/harvard/compare.sh
+++ sources/harvard/compare.sh
@@ -4,10 +4,10 @@
 
 cd $(dirname $0)
 
-qsv join --left position cpssa.csv raw reconciled-positions.csv | qsv select minister,position\[1\],portfolio,startdate,enddate | qsv join --left minister - name reconciled-people.csv | qsv select id,minister,position,portfolio,startdate,enddate | qsv rename item,name,positionItem,position,start,end > scraped.csv
+# qsv join --left position cpssa.csv raw reconciled-positions.csv | qsv select minister,position\[1\],portfolio,startdate,enddate | qsv join --left minister - name reconciled-people.csv | qsv select id,minister,position,portfolio,startdate,enddate | qsv rename item,name,positionItem,position,start,end > scraped.csv
 
-wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | qsv search -s start . > wikidata.csv
+# wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | qsv search -s start . > wikidata.csv
 
-bundle exec ruby diff.rb | qsv sort -s name,position | tee diff.csv
+# bundle exec ruby diff.rb | qsv sort -s name,position | tee diff.csv
 
 cd ~-
