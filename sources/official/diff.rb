9837f5f26bffda3f8f4bf1cc6b38fae0b3bf5d56
Tony Bowden <tony@tmtm.com>
2022-05-31 17:35:17 +0300

    Include Deputy Ministers

--- sources/official/diff.rb
+++ sources/official/diff.rb
@@ -4,7 +4,7 @@
 require 'every_politician_scraper/comparison'
 
 class Comparison < EveryPoliticianScraper::NulllessComparison
-  ACCEPT = %w[Minister Ministra Ministro Ministro-Delegado Ministra-Delegada Secretary Secretario Secretaria]
+  ACCEPT = %w[Minister Ministra Ministro Ministro-Delegado Ministra-Delegada Secretary Secretario Secretaria Deputy Vice-Ministro Vice-Ministra]
 
   def columns
     super + %i[item]
